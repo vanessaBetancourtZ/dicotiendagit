909fdef24fdffcfe5522ceb54c236cdd676f3e25
vanessa <mariettebz90@gmail.com>
2021-12-04 01:58:05 -0500

    asterisco

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'dist/discotiendaweb'));
-app.get('/', function(req,res){
+app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/dist/discotiendaweb/index.html'));
 })
