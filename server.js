8f4771ebf4d42d1c4f1c92995700f5ce8612c88c
vanessa <mariettebz90@gmail.com>
2021-12-04 01:14:23 -0500

    actualizacion

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = express();
 
 app.use(express.static(__dirname+'dist/discotiendaweb'));
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/discotiendaweb/index.htm'));
+    res.sendFile(path.join(__dirname+'/dist/discotiendaweb/index.html'));
 })
